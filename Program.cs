f9d9761f8d90b9fed60281d1df54cb2c9db9cc56
lancelot1022 <32243870+lancelot1022@users.noreply.github.com>
2022-07-15 19:23:22 +0800

    Add files via upload
    Session 1 Homework

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ExerciseOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName;
+            string lastName;
+            int age;
+
+            Console.Write("Enter first name: ");
+            firstName = Console.ReadLine();
+            Console.Write("Enter last name: ");
+            lastName = Console.ReadLine();
+            Console.Write("Enter age: ");
+            age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"My name is {firstName} {lastName}, and I am {age} years old.");
+        }
+    }
+}
